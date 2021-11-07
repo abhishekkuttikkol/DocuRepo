a70464955a34b399a612bc691ba5ac2ef3070b6a
Abhishek <abhishekkuttikkol007@gmail.com>
2021-11-07 15:29:57 +0530

    Final commit

--- src/Components/Create.js
+++ src/Components/Create.js
@@ -123,3 +123,4 @@ const Create = () => {
 };
 
 export default Create;
+  
--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -34,7 +34,7 @@ const Signin = () => {
           <div className="max-w-md w-full space-y-8">
             <div>
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sign Up
+                Sign In
               </h2>
               <p className="mt-2 text-center text-sm text-gray-600">
                 Create an account?{" "}
